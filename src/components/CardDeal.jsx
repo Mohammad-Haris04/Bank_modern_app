d12002bea9cd4f9300441a3f962052d51e418efa
Mohammad-Haris24 <mo.haris7999@gmail.com>
2024-09-10 15:47:06 +0530

    minor bugs resolved

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -9,7 +9,7 @@ const CardDeal = () => {
   const { ref: ref1, inView: inView1 } = useInView({ triggerOnce: true });
   return (
     <section className={`${layout.section}`}>
-      <div className={`${layout.sectionInfo}`}>
+      <div id="product" className={`${layout.sectionInfo}`}>
       <div ref={ref1} className={`animation2 animation-delay-0 ${inView1? 'visible' : ''}`}>
         <h2 className={`${styles.heading2}`}>Find a better card deal <br className="sm:block hidden"/> in few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
